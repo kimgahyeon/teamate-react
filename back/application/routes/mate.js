6a88602122db8adfb870ac49e42b607197ba2434
ch-4ml <fetch@ajou.ac.kr>
2021-09-08 00:39:40 +0900

    update mate route for infinite scroll

--- back/application/routes/mate.js
+++ back/application/routes/mate.js
@@ -35,6 +35,28 @@ router.get("/limit/:limit", async (req, res) => {
   }
 });
 
+router.get("/offset/:offset/limit/:limit", async (req, res) => {
+  const { offset, limit } = req.params;
+  try {
+    const result = await Mate.getMatesByRange(Number(offset), Number(limit));
+    res.status(200).send({ ok: true, mates: result });
+  } catch (err) {
+    console.log(err);
+    res.status(200).send({ ok: false, msg: "Mate를 찾을 수 없습니다." });
+  }
+});
+
+router.get("/role/:role/offset/:offset/limit/:limit", async (req, res) => {
+  const { role, offset, limit } = req.params;
+  try {
+    const result = await Mate.getMatesByRoleAndRange(role, Number(offset), Number(limit));
+    res.status(200).send({ ok: true, mates: result });
+  } catch (err) {
+    console.log(err);
+    res.status(200).send({ ok: false, msg: "Mate를 찾을 수 없습니다." });
+  }
+});
+
 router.get("/:id", async (req, res) => {
   try {
     const mate = await Mate.getMateByID(req.params.id);
